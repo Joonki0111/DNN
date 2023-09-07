62e3f661c7aae6661eaf653bb3fcb3fbe031a67b
JoonKi0111 <135323348+JoonKi0111@users.noreply.github.com>
2023-09-07 16:15:24 +0900

    Add files via upload

--- softmax_regression.cpp
+++ softmax_regression.cpp
@@ -18,10 +18,10 @@ class DNN
     {
         x_batch = x_batch_;
         y_batch = y_batch_;
-        w.assign(y_batch.size(),0);
-        b.assign(y_batch.size(),0);
+        w.assign(y_batch.size(),1);
+        b.assign(y_batch.size(),1);
         output_linear_regression = DNN::learning_function(x_batch,y_batch);
-        output_logistic_regression = DNN::logistic(w,b);
+        output_logistic_regression = DNN::logistic(output_linear_regression.first,output_linear_regression.second);
 
     }
 
@@ -36,32 +36,27 @@ class DNN
     int count_y = 0;
     int count_x = 0;
 
-    // int softmax_epoch = 299999983;
-    int softmax_epoch = 2145544954;
-    double percentage;
+    // int softmax_epoch = 499999984;
+    int softmax_epoch = 10000000;
+    int percentage;
 
     std::pair<std::vector<double>, std::vector<double>> learning_function(const std::vector<std::vector<double>> &x, const std::vector<std::vector<double>> &y)
     {
         for(int i = 0; i < 3; i++)
         {
-            for(int j = 0; j < 9965; j++) //needs to be changed 9965
+            for(int j = 0; j < 999991; j++) //needs to be changed 9965
             {
-                sigma_w_gradient[i] += (2 * std::pow(x[count_y][count_x],2) * w[i]) + (2 * x[count_y][count_x] * b[i]) - (2 * x[count_y][count_x] * y[i][count_y]);
+                sigma_w_gradient[i] += (2 * std::pow(x[count_y][count_x], 2) * w[i]) + (2 * x[count_y][count_x] * b[i]) - (2 * x[count_y][count_x] * y[i][count_y]);
                 sigma_b_gradient[i] += (2 * x[count_y][count_x] * w[i]) - (2 * y[i][count_y]) + (2 * b[i]);
-
-                w[i] -= (0.0001*sigma_w_gradient[i]);
-                b[i] -= (0.0001*sigma_b_gradient[i]);
 
                 if(count_y == (x.size() - 1) && count_x == (x[0].size() - 1))
                 {
                     sigma_w_gradient[i] /= x.size() * x[0].size();
                     sigma_b_gradient[i] /= x.size() * x[0].size();
-                }
-
-                if((i % ((x.size()*x[0].size()) - 1)) == 0)
-                {
-                    sigma_w_gradient[i] = 0;
-                    sigma_b_gradient[i] = 0;
+                    w[i] -= (0.000001*sigma_w_gradient[i]);
+                    b[i] -= (0.000001*sigma_b_gradient[i]);
+                    sigma_w_gradient[i] *= 0;
+                    sigma_b_gradient[i] *= 0;
                 }
 
                 // if((i % 500) == 0)
@@ -82,6 +77,7 @@ class DNN
                 }
                 cost[i] = (w[i] * x[count_y][count_x] + b[i]) - y[i][count_y];
             }
+
             count_y = 0;
             count_x = 0;
         }
@@ -96,31 +92,28 @@ class DNN
 
         for(int i = 0; i < 3; i++)
         {
-            for(int j = 0; j < softmax_epoch; j++) //needs to be changed j
+            for(int j = 0; j < softmax_epoch; j++) //needs to be changed "j"
             {
                 sigma_w_gradient[i] += -((y_batch[i][count_y] - 1 / (1+pow(M_E, -(w[i] * x_batch[count_y][count_x] + b[i])))) * x_batch[count_y][count_x]);
                 sigma_b_gradient[i] += -(y_batch[i][count_y] - 1 / (1+pow(M_E, -(w[i] * x_batch[count_y][count_x] + b[i]))));
     
-                w[i] -= (0.000001*sigma_w_gradient[i]);
-                b[i] -= (0.000001*sigma_b_gradient[i]);
-
-                if(count_y == (x_batch.size() - 1) && count_x == (x_batch[0].size() - 1))
+                if(j % 59 == 0)
                 {
                     sigma_w_gradient[i] /= x_batch.size() * x_batch[0].size();
                     sigma_b_gradient[i] /= x_batch.size() * x_batch[0].size();
-                }
 
-                if((i % ((x_batch.size()*x_batch[0].size()) - 1)) == 0)
-                {
+                    w[i] -= (0.001*sigma_w_gradient[i]);
+                    b[i] -= (0.001*sigma_b_gradient[i]);
+
                     sigma_w_gradient[i] = 0;
                     sigma_b_gradient[i] = 0;
-                }            
+                }
 
-                if((j % 100000) == 0)
+                if((j % 10000000) == 0)
                 {   
                     percentage = (static_cast<double>(j) / softmax_epoch) * 100;
                     // std::cout << percentage << " w : " << w[i] << "           " << "b : " << b[i] << "           "  << "cost :" <<logistic_cost[i] << std::endl << std::endl;
-                    std::cout << i << " " << percentage << "%" << std::endl;
+                    std::cout << "Node number[" << i << "] " << percentage << "%        w : " << w[i] << " b : " << b[i] << " cost : " << logistic_cost[i] << std::endl;
                 }
 
                 count_x ++;
@@ -150,7 +143,7 @@ class DNN
 int main()
 {
     int row = 3;
-    int column = 10; 
+    int column = 20; 
     double count = 1.0;
     double output = 0;
     float num = 0;
@@ -167,6 +160,20 @@ int main()
         }
     }
 
+    std::ofstream outputFile("data.txt");
+
+    if (outputFile.is_open()) {
+        for (int i = 0; i < row; i++) 
+        {
+            outputFile << "Data" << i+1 << " "; 
+            for (int z = 0; z < column; z++) 
+            {
+                outputFile << x_batch[i][z] << " ";
+            }
+            outputFile << "\n"; 
+        }
+    }
+    
     DNN data_obj(x_batch,{{1,0,0},{0,1,0},{0,0,1}});
 
     std::cout << "Linear Regression  : " << std::endl << std::endl;
@@ -187,17 +194,24 @@ int main()
         std::cout << "Final cost : " << data_obj.logistic_cost[i] << std::endl << std::endl;
     }
 
-    std::cout << "Softmax regression : " << std::endl << std::endl;
-    std::cout << "enter data" << std::endl;
+    // std::cout << "Softmax regression : " << std::endl << std::endl;
+    // std::cout << "enter data" << std::endl;
 
-    std::cin >> num;
+    // std::cin >> num;
 
-    for(int i = 0; i < 3; i++)
+    // for(int i = 0; i < 3; i++)
+    // {
+    //     output = 1 / (1 + pow(M_E, -((data_obj.output_logistic_regression.first[i] * num) + data_obj.output_logistic_regression.second[i])));
+
+    //     std::cout << i+1 << " : " << output*100 << "%" << std::endl;
+    // }
+    if (outputFile.is_open()) 
     {
-        output = 1 / (1 + pow(M_E, -((data_obj.output_logistic_regression.first[i] * num) + data_obj.output_logistic_regression.second[i])));
-
-        std::cout << i+1 << " : " << output*100 << "%" << std::endl;
+        for(int i = 0; i < 3; i++)
+        {
+            outputFile << data_obj.output_logistic_regression.first[i] << " " <<data_obj.output_logistic_regression.second[i] << "\n";
+        }
+        outputFile.close();
     }
-
     return 0;
 }
