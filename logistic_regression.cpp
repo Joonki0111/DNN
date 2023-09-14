cf504a74120a7e1d61ef70646cf054e4c74617a1
JoonKi0111 <135323348+JoonKi0111@users.noreply.github.com>
2023-09-14 15:56:49 +0900

    Add files via upload

--- logistic_regression.cpp
+++ logistic_regression.cpp
@@ -14,10 +14,18 @@ class DNN
     double cost;
     double logistic_cost;
 
-    DNN(const std::vector<std::vector<double>> &x_batch_, const std::vector<int> &y_)
+    DNN(const std::vector<std::vector<double>> &x_batch_, const std::vector<int> &y_, const int &linear_epoch_, const int &logistic_epoch_,
+         const float &linear_lr_, const float &logistic_lr_)
     {
         x_batch = x_batch_;
         y = y_;
+
+        linear_epoch = linear_epoch_;
+        logistic_epoch = logistic_epoch;
+
+        linear_lr = linear_lr_;
+        logistic_lr = logistic_lr_;
+
         output_linear_regression = DNN::learning_function(x_batch,y);
         output_logistic_regression = DNN::logistic(w,b);
     }
@@ -26,16 +34,25 @@ class DNN
     private:
     std::vector<std::vector<double>> x_batch;
     std::vector<int> y;
-    double w = 1;
-    double b = 1;
-    double sigma_w_gradient = 1;
-    double sigma_b_gradient = 1;
-    int count_y = 0;
-    int count_x = 0;
+
+    double w;
+    double b;
+
+    double sigma_w_gradient;
+    double sigma_b_gradient;
+
+    int count_y;
+    int count_x;
+
+    int linear_epoch;
+    int logistic_epoch;
+
+    float linear_lr;
+    float logistic_lr;
 
     std::pair<double, double> learning_function(const std::vector<std::vector<double>> &x, const std::vector<int> &y)
     {
-        for(int i = 0; i < 50000; i++)
+        for(int i = 0; i < linear_epoch; i++)
         {
 
             sigma_w_gradient += (2 * std::pow(x[count_y][count_x],2) * w) + (2 * x[count_y][count_x] * b) - (2 * x[count_y][count_x] * y[count_y]);
@@ -47,8 +64,8 @@ class DNN
                 sigma_w_gradient /= x.size() * x[0].size();
                 sigma_b_gradient /= x.size() * x[0].size();
 
-                w = w - (0.0001*sigma_w_gradient);
-                b = b - (0.0001*sigma_b_gradient);
+                w = w - (linear_lr * sigma_w_gradient);
+                b = b - (linear_lr * sigma_b_gradient);
 
                 sigma_w_gradient = 0.0;
                 sigma_b_gradient = 0.0;
@@ -81,7 +98,7 @@ class DNN
         int count_y = 0;
         int count_x = 0;
         
-        for(int i = 0; i < 100000000; i++)
+        for(int i = 0; i < logistic_epoch; i++)
         {
             sigma_w_gradient += -((y[count_y]- 1 / (1+pow(M_E, -(w * x_batch[count_y][count_x] + b)))) * x_batch[count_y][count_x]);
             sigma_b_gradient += -(y[count_y]- 1 / (1+pow(M_E, -(w * x_batch[count_y][count_x] + b))));
@@ -92,8 +109,8 @@ class DNN
                 sigma_w_gradient /= x_batch.size() * x_batch[0].size();
                 sigma_b_gradient /= x_batch.size() * x_batch[0].size();
 
-                w -= (0.001*sigma_w_gradient);
-                b -= (0.001*sigma_b_gradient);
+                w -= (logistic_lr * sigma_w_gradient);
+                b -= (logistic_lr * sigma_b_gradient);
 
                 sigma_w_gradient = 0.0;
                 sigma_b_gradient = 0.0;
@@ -128,7 +145,14 @@ int main()
 {
     int row = 2;
     int column = 10; 
+
     double count = 1.0;
+
+    int linear_epoch = 200000;
+    int logistic_epoch = 10000000;
+
+    float linear_lr = 0.01;
+    float logistic_lr = 0.01;
 
     std::vector<std::vector<double>> x_batch(row,std::vector<double>(column,0));
 
@@ -142,27 +166,31 @@ int main()
         }
     }
 
-    std::ofstream outputFile("data.txt");
+    std::ofstream outputFile("data/logistic_regression_data.txt");
 
-    if (outputFile.is_open()) {
-        for (int i = 0; i < row; i++) {
-            for (int z = 0; z < column; z++) {
-                outputFile << "(" << x_batch[i][z] << "," << i << ")" << "\n";
-            }
-            outputFile << "\n"; 
+    for (int i = 0; i < row; i++) 
+    {
+        for (int z = 0; z < column; z++) {
+            outputFile << "(" << x_batch[i][z] << "," << i << ")" << "\n";
         }
-        outputFile.close();
-    } 
+        outputFile << "\n"; 
+    }
+    
 
-    DNN data_obj(x_batch,{0,1});
+    DNN data_obj(x_batch, {0,1}, linear_epoch, logistic_epoch, linear_lr, logistic_lr);
 
     std::cout << "Linear Regression  : " << std::endl;
     std::cout << "Equation: y = " << data_obj.output_linear_regression.first << "x + " << data_obj.output_linear_regression.second <<std::endl;
     std::cout << "Final cost : " << data_obj.cost << std::endl << std::endl;
 
+    outputFile << "\n" << "linear_regression : y = " << data_obj.output_linear_regression.first << "x + " << data_obj.output_linear_regression.second;
+
     std::cout << "Logistic Regression  : " << std::endl;
     std::cout << "Equation: y = " << data_obj.output_logistic_regression.first << "x + " << data_obj.output_logistic_regression.second <<std::endl;
     std::cout << "Final cost : " << data_obj.logistic_cost << std::endl << std::endl;
+
+    outputFile << "\n" << "logistic_regression : y = " << data_obj.output_logistic_regression.first << "x + " << data_obj.output_logistic_regression.second << "\n";
+    outputFile << "for Desmos : 1/(1+e^{-({" << data_obj.output_logistic_regression.first << "x + " << data_obj.output_logistic_regression.second << "})})";
 
     return 0;
 }
